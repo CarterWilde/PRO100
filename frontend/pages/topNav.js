f194c5d2d3f6bef46e11e3de0b351f541bc70628
Brandon Caballero <60459450+BlokeAble@users.noreply.github.com>
2020-08-06 09:40:19 -0600

    css File added for topNav

--- frontend/pages/topNav.js
+++ frontend/pages/topNav.js
@@ -1,43 +1,5 @@
 export default function topNav() {
-  /*
-  render() 
-  {
-    /*
-    const mystyle = 
-    {
-      topnav {
-        background-color: #333,
-        overflow: hidden
-      }
-      
-       //Style the links inside the navigation bar 
-
-      .topnav a {
-        float: left;
-        color: #f2f2f2;
-        text-align: center;
-        padding: 14px 16px;
-        text-decoration: none;
-        font-size: 17px;
-      }
-      
-       Change the color of links on hover 
-      .topnav a:hover 
-      {
-        background-color: #ddd;
-        color: black;
-      }
-      
-       Add a color to the active/current link 
-      .topnav a.active 
-      {
-        background-color: #4CAF50;
-        color: white;
-      }
-    };
-    
-  }
-  */
+  import '../styles/topNav.css'
   return (
       <div class="topnav">
       <a class ="Logoe" style="" href="#home">Logo</a>

2cd9883ab4cecc6c71213323debbefc8af058edd
Brandon Caballero <60459450+BlokeAble@users.noreply.github.com>
2020-08-06 06:36:27 -0600

    Work Top Nav

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,15 +1,11 @@
-<<<<<<< HEAD
 import Test from "./test"
 import Main from "./main"
-=======
-import Test from "./test";
 
->>>>>>> d0932051669f755e3f10145997346beeca62f95c
 export default function Home() {
   return (
     <div>
-        <Test/>
         <Main/>
+        <Test/>
     </div>
   )
 }
--- frontend/pages/main.js
+++ frontend/pages/main.js
@@ -1,10 +1,48 @@
 export default function Main() {
-    return (
+  
+  render() 
+  {
+    /*
+    const mystyle = 
+    {
+      topnav {
+        background-color: #333,
+        overflow: hidden
+      }
+      
+       //Style the links inside the navigation bar 
+
+      .topnav a {
+        float: left;
+        color: #f2f2f2;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+        font-size: 17px;
+      }
+      
+       Change the color of links on hover 
+      .topnav a:hover 
+      {
+        background-color: #ddd;
+        color: black;
+      }
+      
+       Add a color to the active/current link 
+      .topnav a.active 
+      {
+        background-color: #4CAF50;
+        color: white;
+      }
+    };
+    */
+  return (
       <div class="topnav">
-      <a class="active" href="#home">Home</a>
-      <a href="#news">News</a>
-      <a href="#contact">Contact</a>
-      <a href="#about">About</a>
+      <a class ="Logoe" style="" href="#home">Logo</a>
+      <a href="#Search">Search</a>
+      <a href="#Contact">Notification</a>
+      <a href="#Create">Create New</a>
+      <a href="#Sign In">Sign in</a>
     </div>
 
     )

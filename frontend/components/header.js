e33dd961799c7fcfb758c5048d7d8640181450aa
Carter Cobb <carter.cobb72@gmail.com>
2020-08-07 19:12:48 -0600

    better navbar

--- frontend/components/header.js
+++ frontend/components/header.js
@@ -1,25 +1,37 @@
-import React,{Component} from 'react'
-import {Button, ButtonToolbar} from 'react-bootstrap'
+import React, { Component } from 'react'
+import { Button, ButtonToolbar, Navbar, Nav, Form, FormControl } from 'react-bootstrap'
 import Login from './login'
 
 export default class Header extends Component {
     constructor(props) {
         super(props);
-        this.state = {deps:[], addModalShow:false}
-    }  
+        this.state = { deps: [], addModalShow: false }
+    }
+
+    SearchForPosts = () =>{
+        // fetch and display posts 
+    }
 
     render() {
-        const {deps} = this.state
-        let addModalClose = () => this.setState({addModalShow:false})
+        const { deps } = this.state
+        let addModalClose = () => this.setState({ addModalShow: false })
 
         return (
             <>
-            <ButtonToolbar>
-                <Button variant='primary' className='container' onClick={()=> this.setState({addModalShow:true})}>
-                    Login
-                </Button>
-                <Login show={this.state.addModalShow} onHide={addModalClose}/>
-            </ButtonToolbar>
+                <Navbar bg="dark" variant="dark">
+                    <Navbar.Brand href="#home">Ripple</Navbar.Brand>
+                    <Form inline>
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                        <Button variant="outline-info">Search</Button>
+                    </Form>
+                    <ButtonToolbar inline>
+                        <Button variant='primary' className='container' onClick={() => this.setState({ addModalShow: true })}>
+                            Login
+                        </Button>
+                        <Login show={this.state.addModalShow} onHide={addModalClose} />
+                    </ButtonToolbar>
+                </Navbar>
+
             </>
         )
     }

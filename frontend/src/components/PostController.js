de0b227a5a8b5cc4f4423a6b6dd1c255a2a0e13a
Brandon Caballero <60459450+BlokeAble@users.noreply.github.com>
2020-08-19 10:33:51 -0600

    Post improve

--- frontend/src/components/PostController.js
+++ frontend/src/components/PostController.js
@@ -10,16 +10,15 @@ export default class Login extends Component {
         super(props)
         this.state = {
             show: false,
-            title: null,
-            price: null,
-            descirption: null,
-            imageUrl: null
-
+            title: "Example",
+            price: "$100",
+            descirption: "This is exmaple of what you want to tell the people who see ",
+            imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ0XsKvLjkn5XZG9XP1S2FO0qQp8qO5E6HJ5w&usqp=CAU"
         }
     }
 
     handleClose = () => {
-        this.setState({show: false});
+        this.setState({show: false,imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ0XsKvLjkn5XZG9XP1S2FO0qQp8qO5E6HJ5w&usqp=CAU"});
     }
 
     render() {
@@ -34,13 +33,13 @@ export default class Login extends Component {
                     <Modal.Body>
                         <Stack horizontal horizontalAlign="space-around"  tokens={{childrenGap: 20}}>
                             <Stack verticalAlign="center" tokens={{childrenGap: 20}} style={{width:"50%"}}>
-                                <TextField onChange={(_, newValue) => {this.setState({title: newValue})}}label="Title:" underlined iconProps={titleIcon}/>
+                                <TextField onChange={(_, newValue) => {this.setState({title: newValue})}} maxlength="10" label="Title:" underlined iconProps={titleIcon}/>
                                 <TextField onChange={(_, newValue) => {this.setState({price: newValue})}} label="Price:" prefix='$' underlined iconProps={moneyIcon}/> {/* /^(\d*([.,](?=\d{3}))?\d+)+((?!\2)[.,]\d\d)?$/ */}
                                 <TextField onChange={(_, newValue) => {this.setState({imageUrl: newValue})}}label="Image Url:" underlined iconProps={imageIcon}/>
                                 <TextField onChange={(_, newValue) => {this.setState({descirption: newValue})}} label="Description" multiline resizable={false} style={{height:"200px"}}/>
                             </Stack>
                             <Stack verticalAlign="space-around" horizontalAlign="center" style={{width:"50%"}}>
-                              <Post title={this.state.title} price={this.state.price} descirption ={this.state.descirption}/> 
+                              <Post title={this.state.title} price={this.state.price} descirption ={this.state.descirption} imageUrl ={this.state.imageUrl}/> 
                             </Stack>
                         </Stack>
                     </Modal.Body>

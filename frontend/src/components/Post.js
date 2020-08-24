f70d419b08fe60ce6a9eebb0e9f098a7e4d331b1
Brandon Caballero <60459450+BlokeAble@users.noreply.github.com>
2020-08-24 07:12:21 -0600

    Merged Post

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,13 +1,9 @@
 import * as React from 'react';
 import { Card, ICardTokens, ICardSectionStyles, ICardSectionTokens } from '@uifabric/react-cards';
-<<<<<<< HEAD
 import { FontWeights} from '@uifabric/styling';
 import { Icon, IIconStyles, Image, Stack, IStackTokens, Text, ITextStyles } from 'office-ui-fabric-react';
-
-=======
 import { FontWeights } from '@uifabric/styling';
 import { IconButton, IIconStyles, Image, Stack, IStackTokens, Text, ITextStyles } from 'office-ui-fabric-react';
->>>>>>> e55f112d712c743913e26d76fdc70a995b1aa5a5
 
 /*
 Post By: and buttons 
@@ -33,114 +29,7 @@ const alertClicked = () => {
     alert('Clicked');
 };
 export class Post extends React.Component {
-<<<<<<< HEAD
-  constructor(props) {
-    super(props)
 
-  // TODO: Brandon, The search will return the data needed to complete the post component. The JSON data
-    console.log(this.props.elements) //I added this so you can see it. as of now its only dumby data. It will say only a single string
-} 
-  render() {
-    const titleTextStyles = {
-      root: {
-        color: '#333333',
-        fontSize: 25,
-        fontWeight: FontWeights.regular,
-      },
-    };
-    const priceTextStyles = {
-      root: {
-        color: '#333333',
-        fontSize: 15,
-        fontWeight: FontWeights.regular,
-      },
-    };
-    const descriptionTextStyles = {
-      root: {
-        color: '#333333',
-        fontSize: 12,
-        fontWeight: FontWeights.regular,
-        
-      },
-    };
-
-    const  postByTextStyles = {
-      root: {
-        color: '#333333',
-        fontSize: 10,
-        fontWeight: FontWeights.regular,      
-      },
-    };
-
-    const iconStyles = {
-      root: {
-        color: '#0078D4',
-        fontSize: 16,
-        fontWeight: FontWeights.regular,
-      },
-    };
-    const footerCardSectionStyles = {
-      root: {
-        alignSelf: 'stretch',
-        borderLeft: '1px solid #F3F2F1',
-      },
-    };
-    const infoCardSectionStyles = {
-      root: {
-        alignSelf: 'stretch',
-        borderLeft: '1px solid #F3F2F1',
-      },
-    };
-    
-    const sectionStackTokens = { childrenGap: 20, margin: '5px 5px 5px 5px' };
-    const cardTokens = { childrenMargin: 10};
-    const footerCardSectionTokens = { padding: '0px 0px 0px 12px' };
-    const infoCardSectionTokens = { width: "160px",maxWidth: "160px",maxHeight: "173px",margin: '5px 5px 5px 10px'};
-    const ImageTokens = {maxWidth: "300px",maxHeight: "150px"};
-
-    return (
-      <div  id="post" >
-      <Stack tokens={sectionStackTokens}>
-        <Card aria-label="Clickable horizontal card " horizontal onClick={alertClicked} tokens={cardTokens}>
-
-          <Card.Section styles={infoCardSectionTokens} tokens={infoCardSectionTokens}>
-            <Text maxlength="4"  variant="small" styles={titleTextStyles}>
-             {this.props.title.substring(0,12)}   
-            </Text>
-
-            <Text variant="small" styles={priceTextStyles}>
-            {this.props.price.substring(0,19)}
-            </Text>
-
-            <Text className = "overflow" variant="small" styles={descriptionTextStyles}>
-            {this.props.descirption}
-            </Text>
-          </Card.Section>
-
-          <Card.Item>
-            <div>
-            <Image style={ImageTokens} src= {this.props.imageUrl} />
-             <strong style={postByTextStyles} >By:</strong>
-             </div>
-          </Card.Item>
-
-          <Card.Section styles={footerCardSectionStyles} tokens={footerCardSectionTokens}>
-            <Icon iconName="RedEye" styles={iconStyles}/>
-    
-            <Icon iconName="SingleBookmark" styles={iconStyles} />
-
-            <Stack.Item grow={1}>
-              <span />
-            </Stack.Item>
-            <Icon iconName="MoreVertical" styles={iconStyles} />
-          </Card.Section>
-        </Card>
-      </Stack>
-      </div>
-    );
-  }
-  
-=======
     constructor(props) {
         super(props)
 
@@ -231,6 +120,7 @@ export class Post extends React.Component {
 
                         <Card.Section styles={CardSectionStyles} tokens={footerCardSectionTokens}>
                             <Stack>
+                                <Icon iconName="SingleBookmark" styles={iconStyles} />
                                 <IconButton iconProps={{ iconName: "CaretUpSolid8" }} styles={iconStyles} onClick={() => { console.log("upvote") }} style={{ outline: 'none' }} />
                                 <Text style={{textAlign:"center", fontWeight:FontWeights.bold}}>{this.props.votes}</Text>
                                 <IconButton iconProps={{ iconName: "CaretDownSolid8" }} styles={iconStyles} onClick={() => { console.log("downvote") }} style={{ outline: 'none' }} />
@@ -241,7 +131,5 @@ export class Post extends React.Component {
             </div>
         );
     }
-
->>>>>>> e55f112d712c743913e26d76fdc70a995b1aa5a5
 }
 export default Post;

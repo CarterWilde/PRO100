ba2e8c40f319970d174af3e76a347fc01ce13a32
Carter Cobb <carter.cobb72@gmail.com>
2020-08-27 00:46:11 -0600

    fixed css

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import { Card, ICardTokens, ICardSectionStyles, ICardSectionTokens } from '@uifabric/react-cards';
+import { Card } from '@uifabric/react-cards';
 import { FontWeights } from '@uifabric/styling';
-import { IconButton, Image, Stack, IStackTokens, Text, ITextStyles } from 'office-ui-fabric-react';
+import { IconButton, Image, Stack, Text } from 'office-ui-fabric-react';
 
 
 
@@ -12,90 +12,29 @@ export class Post extends React.Component {
     constructor(props) {
         super(props)
     }
-    render() 
-    {
-        const fontStyle = {
-            color: '#333333',
-            fontWeight: FontWeights.regular,
-        }
-
-        const titleTextStyles = {
-            root: {
-                ...fontStyle,
-                fontSize: 25
-            },
-        };
-        const priceTextStyles = {
-            root: {
-                ...fontStyle,
-                fontSize: 15
-            },
-        };
-        const descriptionTextStyles = {
-            root: {
-                ...fontStyle,
-                fontSize: 12
-
-            },
-        };
-
-        const postByTextStyles = {
-            root: {
-                ...fontStyle,
-                fontSize: 10
-            },
-        };
-
-        const iconStyles = {
-            root: {
-                color: '#0078D4',
-                fontSize: 16,
-                fontWeight: FontWeights.regular,
-            },
-        };
-        const CardSectionStyles = {
-            root: {
-                alignSelf: 'stretch',
-                borderLeft: '1px solid #F3F2F1',
-            },
-        };
-
-        const sectionStackTokens = { childrenGap: 20, margin: '5px 5px 5px 5px' };
-        const cardTokens = { childrenMargin: 10, width: '100%', height:"400px", maxWidth: 'none !important' };
-        const footerCardSectionTokens = { padding: '0' };
-        const infoCardSectionTokens = { width: "160px", padding: '5%' };
-        const ImageTokens = { maxHeight: "150px", padding: '5%' };
-
+    render() {
         return (
             <div id="post" className="lines" >
-                <Stack tokens={sectionStackTokens}>
-                    <Card aria-label="Clickable horizontal card " horizontal tokens={cardTokens} style={{height: "200px",display:"grid", gridTemplateColumns:"5fr 3fr 1fr"}}>
-                        <Card.Section styles={infoCardSectionTokens} tokens={infoCardSectionTokens}>
-                            <Text maxlength="4" variant="small" styles={titleTextStyles}>
-                                {this.props.title}
-                            </Text>
-
-                            <Text variant="small" styles={priceTextStyles}>
-                                {this.props.price}
-                            </Text>
-
-                            <Text className="overflow" variant="small" styles={descriptionTextStyles}>
-                                {this.props.descirption}
-                            </Text>
+                <Stack tokens={{childrenGap:20, margin:"5px"}}>
+                    <Card aria-label="Clickable horizontal card " horizontal tokens={{childrenMargin:10, width:"100%",height:"400px", maxWidth:"none !important"}} style={{ height: "200px", display: "grid", gridTemplateColumns: "5fr 3fr 1fr" }}>
+                        <Card.Section styles={{width:"160px", padding:"5px"}} tokens={{width:"160px", padding:"5px"}}>
+                            <Text maxlength="4" variant="small" styles={{fontWeight:FontWeights.regular, fontSize:25}}>{this.props.title}</Text>
+                            <Text variant="small" styles={{fontWeight:FontWeights.regular, fontSize:15}}>{this.props.price}</Text>
+                            <Text className="overflow" variant="small" styles={{fontWeight:FontWeights.regular, fontSize:12}}>{this.props.descirption}</Text>
                         </Card.Section>
 
                         <Card.Item>
                             <div>
-                                <Image style={ImageTokens} src={this.props.imageUrl} />
-                                <strong style={postByTextStyles} >By: {this.props.username}</strong>
+                                <Image style={{maxHeight:"150px", padding:"5px"}} src={this.props.imageUrl} />
+                                <strong style={{fontWeight:FontWeights.regular, fontSize:10}} >By: {this.props.username}</strong>
                             </div>
                         </Card.Item>
 
-                        <Card.Section style={{display:"grid", justifyContent:"center", alignItems:"center"}} styles={CardSectionStyles} tokens={footerCardSectionTokens}>
+                        <Card.Section style={{ display: "grid", justifyContent: "center", alignItems: "center" }} styles={{alignSelf:"stretch", borderLeft:"1px solid #F3F2F1"}} tokens={{padding:"0px"}}>
                             <Stack width="100%">
-                                <IconButton iconProps={{ iconName: "CaretUpSolid8" }} styles={iconStyles} onClick={() => { console.log("upvote") }} style={{ outline: 'none' }} />
-                                <Text style={{textAlign:"center", fontWeight:FontWeights.bold}}>{this.props.votes}</Text>
-                                <IconButton iconProps={{ iconName: "CaretDownSolid8" }} styles={iconStyles} onClick={() => { console.log("downvote") }} style={{ outline: 'none' }} />
+                                <IconButton iconProps={{ iconName: "CaretUpSolid8" }} style={{color:"#0078D4", fontSize:16,fontWeight:FontWeights.regular}} onClick={() => { console.log("upvote") }} style={{ outline: 'none' }} />
+                                <Text style={{ textAlign: "center", fontWeight: FontWeights.bold }}>{this.props.votes}</Text>
+                                <IconButton iconProps={{ iconName: "CaretDownSolid8" }} style={{color:"#0078D4", fontSize:16,fontWeight:FontWeights.regular}} onClick={() => { console.log("downvote") }} style={{ outline: 'none' }} />
                             </Stack>
                         </Card.Section>
                     </Card>

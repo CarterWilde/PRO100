fb827df0723c99195cd62339a9cd0605268a6858
Carter Cobb <carter.cobb72@gmail.com>
2020-08-28 13:57:30 -0600

    fixed array of users not populating correctly

--- backend/src/Routes.ts
+++ backend/src/Routes.ts
@@ -164,20 +164,18 @@ export const Routes: Route[] = [
         handler: async (req, res) => {
             if(req.body.data.inc === 1) {
                 Post.updateOne({"_id": req.body.data.id}, {
-                    $push: {'Votes.Up': UserModel.default.find({"_id": req.body.data.user})},
+                    $push: {'Votes.Up': await UserModel.default.findOne({"_id": req.body.data.user})},
                     $inc: {'Votes.Total': req.body.data.inc}
                 }, (err, doc)=>{
                     if(err) { console.log(err) }
-                    else { console.log("updated") }
                 })
             }
             else{
                 Post.updateOne({"_id": req.body.data.id}, {
-                    $push: {'Votes.Down': UserModel.default.find({"_id": req.body.data.user})},
+                    $push: {'Votes.Down': await UserModel.default.findOne({"_id": req.body.data.user})},
                     $inc: {'Votes.Total': req.body.data.inc}
                 }, (err, doc)=>{
                     if(err) { console.log(err) }
-                    else { console.log("updated") }
                 })
             }
         }
